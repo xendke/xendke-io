d24976058970e8cbe1cdfb4481c0668a55218e77
Xavier Gomez <xendke@gmail.com>
2018-03-09 23:31:17 -0500

    create react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Grid, Image } from 'semantic-ui-react'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
