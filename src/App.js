9368c3994f75364e3c410e481ae63f5f79cf6b4e
Xavier Gomez <xendke@gmail.com>
2018-03-10 23:01:20 -0500

    build grid for resume segments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Header, Segment } from 'semantic-ui-react';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello World
+      <div className="App">  
+      <Grid columns={2} padded >
+      <Grid.Row streched>
+        <Grid.Column >
+            <Header as='h2' icon='user' content='About Me' attached='top'/>
+            <Segment attached>I am a New York based software developer. I use code to bring ideas to life. I enjoy teaching what I know about code, as well as learning that which I don't. I have knowledge of many tools, and concepts. I have worked on desktop, mobile, and web applications. I am currently focusing on full stack web development. I am available for hire. Please feel free to contact me at me@xendke.io or xendke@gmail.com.</Segment>
+            <Header as='h2' icon='user' content='About Me' attached='top'/>
+            <Segment attached>I am a New York based software developer. I use code to bring ideas to life. I enjoy teaching what I know about code, as well as learning that which I don't. I have knowledge of many tools, and concepts. I have worked on desktop, mobile, and web applications. I am currently focusing on full stack web development. I am available for hire. Please feel free to contact me at me@xendke.io or xendke@gmail.com.</Segment>
+        </Grid.Column>
+
+        <Grid.Column >
+          <Header as='h2' icon='student' content='Education' attached='top'/>
+          <Segment attached>I am a New York based software developer. I use code to bring ideas to life. I enjoy teaching what I know about code, as well as learning that which I don't. I have knowledge of many tools, and concepts. I have worked on desktop, mobile, and web applications. I am currently focusing on full stack web development. I am available for hire. Please feel free to contact me at me@xendke.io or xendke@gmail.com.
+          I am a New York based software developer. I use code to bring ideas to life. I enjoy teaching what I know about code, as well as learning that which I don't. I have knowledge of many tools, and concepts. I have worked on desktop, mobile, and web applications. I am currently focusing on full stack web development. I am available for hire. Please feel free to contact me at me@xendke.io or xendke@gmail.com.</Segment>
+          <Header as='h2' icon='student' content='Education' attached='top'/>
+          <Segment attached>I am a New York based software developer. I use code to bring ideas to life. I enjoy teaching what I know about code, as well as learning that which I don't. I have knowledge of many tools, and concepts. I have worked on desktop, mobile, and web applications. I am currently focusing on full stack web development. I am available for hire. Please feel free to contact me at me@xendke.io or xendke@gmail.com.
+          I am a New York based software developer. I use code to bring ideas to life. I enjoy teaching what I know about code, as well as learning that which I don't. I have knowledge of many tools, and concepts. I have worked on desktop, mobile, and web applications. I am currently focusing on full stack web development. I am available for hire. Please feel free to contact me at me@xendke.io or xendke@gmail.com.</Segment>
+        </Grid.Column>
+        </Grid.Row>
+      </Grid>
       </div>
     );
   }
