398f3d593163152f8ad4d9a3317647afd8568dd3
Xavier Gomez <xendke@gmail.com>
2018-03-14 19:46:49 -0400

    added resume link

--- src/components/Cards/AboutMe.jsx
+++ src/components/Cards/AboutMe.jsx
@@ -6,12 +6,19 @@ class AboutMe extends Component {
     render() {
         return (
             <Card title="About Me" icon="user">
-                I am a New York based software developer. I use code to bring ideas to life. I enjoy teaching what I know about code, as well as learning that which I don't. I have knowledge of many tools, and concepts. I have worked on desktop, mobile, and web applications. I am currently focusing on full stack web development. I am available for hire. Please feel free to contact me at me@xendke.io or xendke@gmail.com.
-                <br/>
-                <Icon.Group>
-                    <Icon link name="file text outline"/>
-                    Resume
-                </Icon.Group>
+                <p>
+                    I am a New York based software developer. I use code to bring ideas to life. I enjoy teaching what I know about code, as well as learning that which I don't. 
+                </p>
+
+                <p>
+                    I have knowledge of many tools, and concepts. I have worked on desktop, mobile, and web applications. I am currently focusing on full stack web development. 
+                </p>
+
+                <p>
+                I am available for hire. Please feel free to contact me at me@xendke.io or xendke@gmail.com.
+                </p>
+                <Icon link name="file text outline"/>
+                <a href= "https://drive.google.com/uc?export=download&id=0B4-jhWx3RgYreS1hTkoxNkRTZG8">Resume</a>
             </Card>
         )
     }
