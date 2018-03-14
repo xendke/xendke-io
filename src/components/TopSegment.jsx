93cd4b5d28f1e56b960a8f8379211abe2cedca7d
Xavier Gomez <xendke@gmail.com>
2018-03-14 18:16:08 -0400

    Responsive name in TopSegment

--- src/components/TopSegment.jsx
+++ src/components/TopSegment.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Image, Container, List } from 'semantic-ui-react';
+import { Grid, Image, Container, List, Responsive } from 'semantic-ui-react';
 
 
 class TopSegment extends Component {
@@ -13,7 +13,12 @@ class TopSegment extends Component {
                 <Container textAlign="left">
                 <List>
                     <List.Item icon='user'> 
-                        <List.Header as="h2">Juan X. Gomez</List.Header>
+                        <Responsive as="h2" minWidth={759}>
+                            Juan X. Gomez
+                        </Responsive>
+                        <Responsive as="h2" maxWidth={758} style={{fontSize: 20}}>
+                            Juan X. Gomez
+                        </Responsive>
                     </List.Item>
                     <List.Item icon='marker' content='White Plains, NY' />
                     <List.Item icon='phone' content='(914) 565-1708' />
