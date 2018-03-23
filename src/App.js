5b412718c2a0a7795119c38056ad0962e01f9259
Xavier Gomez <xendke@gmail.com>
2018-03-23 10:46:55 -0400

    move pages to their own component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Grid, Divider, Container } from 'semantic-ui-react';
-import { AboutMe, Education, Links, Experience, Recognitions } from './components/Cards';
+import { Divider, Container, Menu } from 'semantic-ui-react';
 import TopSegment from './components/TopSegment.jsx';
+import Resume from './components/Resume.jsx';
+import Projects from './components/Projects.jsx';
+
 
 class App extends Component {
+  state = { activePage: "resume" }
+
+  handleItemClick = (e, { name }) => this.setState({ activePage: name })
+
+  constructor() {
+    super();
+    // resume is the only page visible
+    this.visibilities = {resume: {display: "none"}, projects: {display: "none"}, thoughts: {display: "none"}};
+    this.visibilities[this.state.activePage].display = "block";
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    // reset the visibilities to none
+    this.visibilities = {resume: {display: "none"}, projects: {display: "none"}, thoughts: {display: "none"}};
+    const { activePage } = nextState;
+    // make the soon to be active page visible
+    this.visibilities[activePage].display = "block";
+  }
+
   render() {
+    const { activePage } = this.state;
+
     return (
       <div className="App">  
         <TopSegment/>
         <Divider horizontal>xendke.io</Divider>
         <Container>
-          <Grid columns={2} stackable padded>
-            <Grid.Row streched >
-              <Grid.Column>
-                <AboutMe/>
-                <Experience />
-              </Grid.Column>
+          <Menu widths={3}>
+            <Menu.Item name="resume" active={activePage === "resume"} onClick={this.handleItemClick} />
+            <Menu.Item name="projects" active={activePage === "projects"} onClick={this.handleItemClick} />
+            <Menu.Item name="thoughts" active={activePage === "thoughts"} onClick={this.handleItemClick} />
+          </Menu>
 
-              <Grid.Column>
-                <Education/>
-                <Links/>
-                <Recognitions/>
-              </Grid.Column>
+          <Resume visibility={this.visibilities.resume}/>
 
-            </Grid.Row >
-          </Grid>
+          <Projects visibility={this.visibilities.projects}/>          
+
         </Container>
         <Divider/>
         <Container textAlign='center' style={{paddingBottom: 15, color: "grey"}}>
