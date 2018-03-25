f6942d3adb4e53e108123b3cc1dee93cb3ad4076
Xavier Gomez <xendke@gmail.com>
2018-03-25 16:37:17 -0400

    better github widget

--- src/components/Feeds.jsx
+++ src/components/Feeds.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Grid } from 'semantic-ui-react';
+import "./Feeds.css";
 
 class Feeds extends Component {
     componentDidMount() {
@@ -12,29 +13,25 @@ class Feeds extends Component {
         twitter.setAttribute('async', "true");
         document.body.appendChild(twitter);
 
-        const github = document.createElement('iframe');
-        github.setAttribute(
-            'src', 
-            'http://colmdoyle.github.io/gh-activity/gh-activity.html?user=xendke&type=user'
-        );
-        github.setAttribute('frameborder', 0);
-        github.setAttribute('width', "100%");
-        github.setAttribute('height', 400);
-
-        var container = document.getElementById("github-widget-container");
-        container.appendChild(github);
+        window.GitHubActivity.feed({
+            username: "xendke",
+            selector: "#github-widget-container",
+            limit: 20 
+        });
     }
 
     render() {
+        console.log("feeds rendered");
         return (
             <Container style={this.props.visibility}>
                 <Grid columns={2} padded stackable>
-                <Grid.Row stretched>
-                    <Grid.Column id="github-widget-container">
-                        {/* github widget */}
+                <Grid.Row>
+                    <Grid.Column >
+                        <div id="github-widget-container">
+                        </div>
                     </Grid.Column>
                     <Grid.Column>
-                        <a class="twitter-timeline" href="https://twitter.com/xendke?ref_src=twsrc%5Etfw" data-height="400">Tweets by xendke</a> 
+                        <a className="twitter-timeline" href="https://twitter.com/xendke?ref_src=twsrc%5Etfw" data-height="410">Tweets by xendke</a> 
                     </Grid.Column>
                  </Grid.Row>
                 </Grid>
